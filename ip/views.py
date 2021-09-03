3f9fae62e9f5bffd89b64a96b00a1dad71cbc80b
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-09-03 13:09:38 +0530

    Added functionality to view map and display IP along with full address.

--- ip/views.py
+++ ip/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+import requests
+import folium
 
 # Create your views here.
 def home(request):
-    return render(request,'ip/home.html')
+    if request.method == 'POST':
+        ip_addr = requests.get("https://api.ipify.org").text
+        api_key = "5be5f0b470c4d878afed50b1c21a0940"
+        location_details = requests.get(f"http://api.ipstack.com/{ip_addr}?access_key={api_key}").json()
+        latitude = location_details["latitude"]
+        longitude = location_details["longitude"]
+        location = f"{location_details['city']}, {location_details['region_name']}, {location_details['country_name']}, PINCODE - {location_details['zip']}"
+        worldmap = folium.Map()
+        folium.Marker([latitude,longitude]).add_to(worldmap)
+        htmlcode = worldmap._repr_html_()
+        context = {'map':htmlcode, 'location':location, 'ip':ip_addr}
+        return render(request,'ip/home.html',context)
+    if request.method == 'GET':
+        return render(request,'ip/home.html')
